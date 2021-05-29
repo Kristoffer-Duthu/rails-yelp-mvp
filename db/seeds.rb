aba7208ac1440b07d8b2771c589f4f05cd1c19b7
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-05-29 15:40:13 +0800

    seed of database, show and index pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  res = Restaurant.create!(
+    name: Faker::JapaneseMedia::SwordArtOnline.real_name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  rev = Review.create!(
+    content: Faker::TvShows::SouthPark.quote,
+    rating: rand(1..5),
+    restaurant: res
+  )
+end
